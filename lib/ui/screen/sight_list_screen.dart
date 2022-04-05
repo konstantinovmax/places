dd96bc859099abaf5374c86f63bc942341606d4e
Maxim <konstantinovmax@gmail.com>
2022-04-05 12:33:25 +0300

    Add appbar

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,8 +10,25 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hello!')),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80.0,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: const Text(
+          'Список\nинтересных мест',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.w700,
+            fontSize: 32.0,
+            height: 1.12,
+            color: Color(0xFF252849),
+          ),
+          maxLines: 2,
+        ),
+        centerTitle: false,
+      ),
+      body: const Center(child: Text('Hello!')),
     );
   }
 }
