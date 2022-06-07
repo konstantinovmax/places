7700f14f2574e4d5390d74e3405ba00028527ee8
Maxim <konstantinovmax@gmail.com>
2022-06-07 16:42:37 +0300

    feat: Add filter selection functionality

--- lib/ui/screens/filters_screen.dart
+++ lib/ui/screens/filters_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:places/domain/my_coordinates.dart';
+import 'package:places/domain/sight.dart';
 import 'package:places/mocks.dart';
 import 'package:places/theme/app_assets.dart';
 import 'package:places/theme/app_colors.dart';
@@ -8,8 +9,29 @@ import 'package:places/theme/app_strings.dart';
 import 'package:places/theme/app_typography.dart';
 import 'dart:math' as math;
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var myCoordinates = MyCoordinates(lat: 59.7722654, lon: 30.3239842);
+  var selectedRange = RangeValues(100, 10000);
+  List<Sight> filteredPlaces = [];
+  bool isHotelSelected = false;
+  bool isRestaurantSelected = false;
+  bool isSpecialPlaceSelected = false;
+  bool isParkSelected = false;
+  bool isMuseumSelected = false;
+  bool isCafeSelected = false;
+
+  @override
+  void initState() {
+    filteringSightByDistance();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +50,9 @@ class FiltersScreen extends StatelessWidget {
         ),
         actions: [
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              rejectCategories();
+            },
             style: TextButton.styleFrom(padding: EdgeInsets.zero),
             child: Text(
               AppStrings.clear,
@@ -44,23 +68,148 @@ class FiltersScreen extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 24.0),
-            FiltersScreenCategories(),
+            FiltersScreenCategories(
+              isHotelSelected: isHotelSelected,
+              isRestaurantSelected: isRestaurantSelected,
+              isSpecialPlaceSelected: isSpecialPlaceSelected,
+              isParkSelected: isParkSelected,
+              isMuseumSelected: isMuseumSelected,
+              isCafeSelected: isCafeSelected,
+              selectHotelToggler: selectHotelToggler,
+              selectRestaurantToggler: selectRestaurantToggler,
+              selectSpecialPlaceToggler: selectSpecialPlaceToggler,
+              selectParkToggler: selectParkToggler,
+              selectMuseumToggler: selectMuseumToggler,
+              selectCafeToggler: selectCafeToggler,
+            ),
             const SizedBox(height: 56.0),
-            FiltersScreenSlider(),
+            FiltersScreenSlider(
+              filteredPlaces: filteredPlaces,
+              filteringFunction: filteringSightByDistance,
+              selectedRange: selectedRange,
+              updateSelectedRange: updateSelectedRange,
+            ),
           ],
         ),
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: FiltersScreenSubmitButton(theme: theme),
+        child: FiltersScreenSubmitButton(
+          theme: theme,
+          filteredPlaces: filteredPlaces,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+
+  void rejectCategories() {
+    setState(() {
+      isHotelSelected = false;
+      isRestaurantSelected = false;
+      isSpecialPlaceSelected = false;
+      isParkSelected = false;
+      isMuseumSelected = false;
+      isCafeSelected = false;
+    });
+  }
+
+  void selectHotelToggler() {
+    setState(() {
+      isHotelSelected = !isHotelSelected;
+    });
+  }
+
+  void selectRestaurantToggler() {
+    setState(() {
+      isRestaurantSelected = !isRestaurantSelected;
+    });
+  }
+
+  void selectSpecialPlaceToggler() {
+    setState(() {
+      isSpecialPlaceSelected = !isSpecialPlaceSelected;
+    });
+  }
+
+  void selectParkToggler() {
+    setState(() {
+      isParkSelected = !isParkSelected;
+    });
+  }
+
+  void selectMuseumToggler() {
+    setState(() {
+      isMuseumSelected = !isMuseumSelected;
+    });
+  }
+
+  void selectCafeToggler() {
+    setState(() {
+      isCafeSelected = !isCafeSelected;
+    });
+  }
+
+  void updateSelectedRange(RangeValues newRange) {
+    setState(() {
+      selectedRange = newRange;
+    });
+    filteringSightByDistance();
+  }
+
+  void filteringSightByDistance() {
+    mocks.forEach((place) {
+      bool isNear = sightsNear(place, myCoordinates, selectedRange);
+      if (isNear && !filteredPlaces.contains(place)) {
+        setState(() {
+          filteredPlaces.add(place);
+        });
+      }
+    });
+  }
+
+  bool sightsNear(
+    Sight sight,
+    MyCoordinates myPosition,
+    RangeValues selectedRange,
+  ) {
+    var ky = 4000000 / 360;
+    var kx = math.cos(math.pi * myPosition.lat / 180.0) * ky;
+    var dx = (myPosition.lon - sight.lon).abs() * kx;
+    var dy = (myPosition.lat - sight.lat).abs() * ky;
+    var squareRoot = math.sqrt(dx * dx + dy * dy);
+    return squareRoot >= selectedRange.start && squareRoot <= selectedRange.end;
+  }
 }
 
 class FiltersScreenCategories extends StatelessWidget {
-  const FiltersScreenCategories({Key? key}) : super(key: key);
+  final bool isHotelSelected;
+  final bool isRestaurantSelected;
+  final bool isSpecialPlaceSelected;
+  final bool isParkSelected;
+  final bool isMuseumSelected;
+  final bool isCafeSelected;
+  final Function selectHotelToggler;
+  final Function selectRestaurantToggler;
+  final Function selectSpecialPlaceToggler;
+  final Function selectParkToggler;
+  final Function selectMuseumToggler;
+  final Function selectCafeToggler;
+  const FiltersScreenCategories({
+    Key? key,
+    required this.isHotelSelected,
+    required this.isRestaurantSelected,
+    required this.isSpecialPlaceSelected,
+    required this.isParkSelected,
+    required this.isMuseumSelected,
+    required this.isCafeSelected,
+    required this.selectHotelToggler,
+    required this.selectRestaurantToggler,
+    required this.selectSpecialPlaceToggler,
+    required this.selectParkToggler,
+    required this.selectMuseumToggler,
+    required this.selectCafeToggler,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -83,26 +232,38 @@ class FiltersScreenCategories extends StatelessWidget {
             FiltersScreenCategory(
               icon: AppAssets.hotelIcon,
               label: AppStrings.hotel,
+              isSelected: isHotelSelected,
+              selectCategoryToggler: selectHotelToggler,
             ),
             FiltersScreenCategory(
               icon: AppAssets.restaurantIcon,
               label: AppStrings.restaurant,
+              isSelected: isRestaurantSelected,
+              selectCategoryToggler: selectRestaurantToggler,
             ),
             FiltersScreenCategory(
               icon: AppAssets.specialPlaceIcon,
               label: AppStrings.specialPlace,
+              isSelected: isSpecialPlaceSelected,
+              selectCategoryToggler: selectSpecialPlaceToggler,
             ),
             FiltersScreenCategory(
               icon: AppAssets.parkIcon,
               label: AppStrings.park,
+              isSelected: isParkSelected,
+              selectCategoryToggler: selectParkToggler,
             ),
             FiltersScreenCategory(
               icon: AppAssets.museumIcon,
               label: AppStrings.museum,
+              isSelected: isMuseumSelected,
+              selectCategoryToggler: selectMuseumToggler,
             ),
             FiltersScreenCategory(
               icon: AppAssets.cafeIcon,
               label: AppStrings.cafe,
+              isSelected: isCafeSelected,
+              selectCategoryToggler: selectCafeToggler,
             ),
           ],
         ),
@@ -111,27 +272,18 @@ class FiltersScreenCategories extends StatelessWidget {
   }
 }
 
-class FiltersScreenCategory extends StatefulWidget {
+class FiltersScreenCategory extends StatelessWidget {
   final String icon;
   final String label;
+  final bool isSelected;
+  final Function selectCategoryToggler;
   const FiltersScreenCategory({
     Key? key,
     required this.icon,
     required this.label,
+    required this.isSelected,
+    required this.selectCategoryToggler,
   }) : super(key: key);
-
-  @override
-  State<FiltersScreenCategory> createState() => _FiltersScreenCategoryState();
-}
-
-class _FiltersScreenCategoryState extends State<FiltersScreenCategory> {
-  late bool isMarked;
-
-  @override
-  void initState() {
-    deselectFilter();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -142,9 +294,7 @@ class _FiltersScreenCategoryState extends State<FiltersScreenCategory> {
         children: [
           InkWell(
             onTap: () {
-              setState(() {
-                isMarked = !isMarked;
-              });
+              selectCategoryToggler();
             },
             borderRadius: BorderRadius.all(Radius.circular(50.0)),
             child: SizedBox(
@@ -161,12 +311,12 @@ class _FiltersScreenCategoryState extends State<FiltersScreenCategory> {
                     ),
                     child: Center(
                         child: SvgPicture.asset(
-                      widget.icon,
+                      icon,
                       color: AppColors.fruitSaladColor,
                       fit: BoxFit.contain,
                     )),
                   ),
-                  if (isMarked)
+                  if (isSelected)
                     Align(
                       alignment: Alignment.bottomRight,
                       child: Container(
@@ -184,27 +334,25 @@ class _FiltersScreenCategoryState extends State<FiltersScreenCategory> {
             ),
           ),
           const SizedBox(height: 12.0),
-          Text(widget.label, style: AppTypography.text12RegularOxfordBlue),
+          Text(label, style: AppTypography.text12RegularOxfordBlue),
         ],
       ),
     );
   }
-
-  void deselectFilter() {
-    isMarked = false;
-  }
 }
 
-class FiltersScreenSlider extends StatefulWidget {
-  const FiltersScreenSlider({Key? key}) : super(key: key);
-
-  @override
-  State<FiltersScreenSlider> createState() => _FiltersScreenSliderState();
-}
-
-class _FiltersScreenSliderState extends State<FiltersScreenSlider> {
-  var selectedRange = RangeValues(100, 10000);
-  var myCoordinates = MyCoordinates(lat: 59.7722654, lon: 30.3239842);
+class FiltersScreenSlider extends StatelessWidget {
+  final List<Sight> filteredPlaces;
+  final Function filteringFunction;
+  final RangeValues selectedRange;
+  final Function updateSelectedRange;
+  const FiltersScreenSlider({
+    Key? key,
+    required this.filteredPlaces,
+    required this.filteringFunction,
+    required this.selectedRange,
+    required this.updateSelectedRange,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -236,9 +384,8 @@ class _FiltersScreenSliderState extends State<FiltersScreenSlider> {
           child: RangeSlider(
             values: selectedRange,
             onChanged: (newRange) {
-              setState(() {
-                selectedRange = newRange;
-              });
+              updateSelectedRange(newRange);
+              filteringFunction();
             },
             min: 100,
             max: 10000,
@@ -247,20 +394,17 @@ class _FiltersScreenSliderState extends State<FiltersScreenSlider> {
       ],
     );
   }
-
-  void sightsNear(checkPoint, centerPoint, km) {
-    var ky = 40000000 / 360;
-    var kx = math.cos(math.pi * centerPoint.lat / 180.0) * ky;
-    var dx = (centerPoint.lon - checkPoint.lon).abs() * kx;
-    var dy = (centerPoint.lat - checkPoint.lat).abs() * ky;
-    print(math.sqrt(dx * dx + dy * dy) <= km);
-  }
 }
 
 class FiltersScreenSubmitButton extends StatelessWidget {
   final ThemeData theme;
-  const FiltersScreenSubmitButton({Key? key, required this.theme})
-      : super(key: key);
+  final List<Sight> filteredPlaces;
+
+  const FiltersScreenSubmitButton({
+    Key? key,
+    required this.theme,
+    required this.filteredPlaces,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -285,7 +429,7 @@ class FiltersScreenSubmitButton extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              AppStrings.show.toUpperCase(),
+              '${AppStrings.show.toUpperCase()} (${filteredPlaces.length})',
               style: theme.textTheme.subtitle1,
             ),
           ],
